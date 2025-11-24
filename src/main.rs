441d4d51cb95a7bed9b1506353b9b72b5476b375
Keiran <keircn@proton.me>
2025-11-24 14:35:48 +0000

    implement xdg_shell, wl_*

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use wayland_server::{Display, GlobalDispatch, ListeningSocket};
-use wayland_server::protocol::wl_compositor::{self, WlCompositor};
-use wayland_server::protocol::wl_surface::{self, WlSurface};
+use wayland_server::protocol::{
+    wl_compositor::{self, WlCompositor},
+    wl_surface::{self, WlSurface},
+    wl_seat::{self, WlSeat},
+    wl_output::{self, WlOutput},
+    wl_shm::{self, WlShm},
+    wl_shm_pool::{self, WlShmPool},
+    wl_buffer::{self, WlBuffer},
+};
+use wayland_protocols::xdg::shell::server::{
+    xdg_wm_base::{self, XdgWmBase},
+    xdg_surface::{self, XdgSurface},
+    xdg_toplevel::{self, XdgToplevel},
+    xdg_popup::{self, XdgPopup},
+    xdg_positioner::{self, XdgPositioner},
+};
 use std::sync::Arc;
 
 struct State;
@@ -49,11 +63,230 @@ impl wayland_server::Dispatch<WlSurface, ()> for State {
     ) {}
 }
 
+impl GlobalDispatch<XdgWmBase, ()> for State {
+    fn bind(
+        _state: &mut Self,
+        _handle: &wayland_server::DisplayHandle,
+        _client: &wayland_server::Client,
+        resource: wayland_server::New<XdgWmBase>,
+        _global_data: &(),
+        data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {
+        data_init.init(resource, ());
+    }
+}
+
+impl wayland_server::Dispatch<XdgWmBase, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &XdgWmBase,
+        request: xdg_wm_base::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {
+        match request {
+            xdg_wm_base::Request::CreatePositioner { id } => {
+                data_init.init(id, ());
+            }
+            xdg_wm_base::Request::GetXdgSurface { id, .. } => {
+                data_init.init(id, ());
+            }
+            _ => {}
+        }
+    }
+}
+
+impl wayland_server::Dispatch<XdgPositioner, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &XdgPositioner,
+        _request: xdg_positioner::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        _data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {}
+}
+
+impl wayland_server::Dispatch<XdgSurface, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &XdgSurface,
+        request: xdg_surface::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {
+        match request {
+            xdg_surface::Request::GetToplevel { id } => {
+                data_init.init(id, ());
+            }
+            xdg_surface::Request::GetPopup { id, .. } => {
+                data_init.init(id, ());
+            }
+            _ => {}
+        }
+    }
+}
+
+impl wayland_server::Dispatch<XdgToplevel, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &XdgToplevel,
+        _request: xdg_toplevel::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        _data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {}
+}
+
+impl wayland_server::Dispatch<XdgPopup, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &XdgPopup,
+        _request: xdg_popup::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        _data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {}
+}
+
+impl GlobalDispatch<WlSeat, ()> for State {
+    fn bind(
+        _state: &mut Self,
+        _handle: &wayland_server::DisplayHandle,
+        _client: &wayland_server::Client,
+        resource: wayland_server::New<WlSeat>,
+        _global_data: &(),
+        data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {
+        let seat = data_init.init(resource, ());
+        seat.capabilities(wl_seat::Capability::Pointer | wl_seat::Capability::Keyboard);
+    }
+}
+
+impl wayland_server::Dispatch<WlSeat, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &WlSeat,
+        _request: wl_seat::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        _data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {}
+}
+
+impl GlobalDispatch<WlOutput, ()> for State {
+    fn bind(
+        _state: &mut Self,
+        _handle: &wayland_server::DisplayHandle,
+        _client: &wayland_server::Client,
+        resource: wayland_server::New<WlOutput>,
+        _global_data: &(),
+        data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {
+        let output = data_init.init(resource, ());
+        output.geometry(0, 0, 1920, 1080, wl_output::Subpixel::Unknown,
+            "Minimal".into(), "Compositor".into(), wl_output::Transform::Normal);
+        output.mode(wl_output::Mode::Current | wl_output::Mode::Preferred, 1920, 1080, 60000);
+        if output.version() >= 2 {
+            output.done();
+        }
+    }
+}
+
+impl wayland_server::Dispatch<WlOutput, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &WlOutput,
+        _request: wl_output::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        _data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {}
+}
+
+impl GlobalDispatch<WlShm, ()> for State {
+    fn bind(
+        _state: &mut Self,
+        _handle: &wayland_server::DisplayHandle,
+        _client: &wayland_server::Client,
+        resource: wayland_server::New<WlShm>,
+        _global_data: &(),
+        data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {
+        let shm = data_init.init(resource, ());
+        shm.format(wl_shm::Format::Argb8888);
+        shm.format(wl_shm::Format::Xrgb8888);
+    }
+}
+
+impl wayland_server::Dispatch<WlShm, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &WlShm,
+        request: wl_shm::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {
+        match request {
+            wl_shm::Request::CreatePool { id, .. } => {
+                data_init.init(id, ());
+            }
+            _ => {}
+        }
+    }
+}
+
+impl wayland_server::Dispatch<WlShmPool, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &WlShmPool,
+        request: wl_shm_pool::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {
+        match request {
+            wl_shm_pool::Request::CreateBuffer { id, .. } => {
+                data_init.init(id, ());
+            }
+            _ => {}
+        }
+    }
+}
+
+impl wayland_server::Dispatch<WlBuffer, ()> for State {
+    fn request(
+        _state: &mut Self,
+        _client: &wayland_server::Client,
+        _resource: &WlBuffer,
+        _request: wl_buffer::Request,
+        _data: &(),
+        _dhandle: &wayland_server::DisplayHandle,
+        _data_init: &mut wayland_server::DataInit<'_, Self>,
+    ) {}
+}
+
 fn main() {
     let mut display = Display::<State>::new().expect("Failed to create display");
     let dh = display.handle();
     
     dh.create_global::<State, WlCompositor, _>(6, ());
+    dh.create_global::<State, XdgWmBase, _>(5, ());
+    dh.create_global::<State, WlSeat, _>(7, ());
+    dh.create_global::<State, WlOutput, _>(4, ());
+    dh.create_global::<State, WlShm, _>(1, ());
 
     let socket = ListeningSocket::bind_auto("wayland", 0..32)
         .expect("Failed to create socket");
